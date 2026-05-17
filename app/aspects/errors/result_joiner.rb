8ff3ac584dd317154699a8e158173a3aff223698
Brooke Kuhlmann <brooke@alchemists.io>
2026-05-17 15:35:56 -0600

    Added error result joiner
    Joins multiple result errors into single message. This will soon be used to render a single flash message.
    
    Milestone: minor

---
+++ app/aspects/errors/result_joiner.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require "refinements/array"
+require "refinements/hash"
+
+module Terminus
+  module Aspects
+    # Joins multiple error messages from a Dry Schema/Validation result into a single sentence.
+    module Errors
+      using Refinements::Array
+      using Refinements::Hash
+
+      ResultJoiner = lambda do |subject, result|
+        messages = result.errors.to_h.each.with_object [] do |(key, value), all|
+          if value.is_a? Hash
+            value.flatten_keys!(delimiter: ".").each do |sub_key, sub_value|
+              all.append "#{key}.#{sub_key} #{sub_value.to_sentence}"
+            end
+          else
+            all.append "#{key} #{value.to_sentence}"
+          end
+        end
+
+        "#{subject} #{messages.to_sentence}."
+      end
+    end
+  end
+end
