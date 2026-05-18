4e51cd170c1e470ba4d385ca2f3a595ea80894f2
Brooke Kuhlmann <brooke@alchemists.io>
2026-05-18 09:41:06 -0600

    Added zipper
    Wraps `Zip::OutputStream` to create an in memory zip file in order to answer a monad result for further processing.
    
    Milestone: minor

---
+++ spec/app/aspects/zipper_spec.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require "hanami_helper"
+
+RSpec.describe Terminus::Aspects::Zipper do
+  subject(:zipper) { described_class.new }
+
+  describe "#call" do
+    let(:manifest) { {"one.txt" => "One", "two.txt" => "Two"} }
+
+    it "create zip file in memory" do
+      io = zipper.call(manifest).value!
+
+      content = Zip::File.open_buffer(io).each.with_object({}) do |entry, attributes|
+        attributes[entry.name] = entry.get_input_stream.read
+      end
+
+      expect(content).to eq(manifest)
+    end
+
+    it "answers StringIO instance" do
+      expect(zipper.call(manifest)).to match(Success(kind_of(StringIO)))
+    end
+
+    it "answers failure with invalid type" do
+      expect(zipper.call({bogus: Object.new})).to be_failure(
+        "No implicit conversion of Object into String"
+      )
+    end
+
+    it "answers failure with zip error" do
+      io = class_double Zip::OutputStream
+      zipper = described_class.new(io:)
+
+      allow(io).to receive(:write_buffer).and_raise(Zip::Error, "Danger!")
+
+      expect(zipper.call({test: "test"})).to be_failure("Danger!")
+    end
+  end
+end
